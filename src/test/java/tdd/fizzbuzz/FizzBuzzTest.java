7855ab5b476a75f16105a1cc63347cff1a55942a
deverca <deverca@oocl.com>
2021-08-05 13:41:21 +0800

    [FIZZBUZZ][CARMELA] test: added test case should_return_order_number_when_count_off_given_normal_case_order_number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,15 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static  org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_return_order_number_when_count_off_given_normal_case_order_number(){
+        int orderNumber = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.countOff(orderNumber);
+        assertThat(result).isEqualTo("1");
+    }
 }
