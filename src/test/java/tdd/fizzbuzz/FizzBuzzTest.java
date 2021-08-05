8306acd3e5af43d0035f6ed55932dade5d0f3aeb
deverca <deverca@oocl.com>
2021-08-05 13:44:14 +0800

    [FIZZBUZZ][CARMELA] test: added test case should_return_Fizz_when_count_off_given_multiple_by_three_order_number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -4,12 +4,19 @@ import org.junit.jupiter.api.Test;
 import static  org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class FizzBuzzTest {
-
+    FizzBuzz fizzBuzz = new FizzBuzz();
     @Test
     void should_return_order_number_when_count_off_given_normal_case_order_number(){
         int orderNumber = 1;
-        FizzBuzz fizzBuzz = new FizzBuzz();
+
         String result = fizzBuzz.countOff(orderNumber);
         assertThat(result).isEqualTo("1");
+    }
+
+    @Test
+    void should_return_Fizz_when_count_off_given_multiple_by_three_order_number(){
+        int orderNumber = 3;
+        String result = fizzBuzz.countOff(orderNumber);
+        assertThat(result).isEqualTo("Fizz");
     }
 }
