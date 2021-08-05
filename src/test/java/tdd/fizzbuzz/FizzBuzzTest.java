962ea22ebd99ce572ec9392133163500dd29dcb5
deverca <deverca@oocl.com>
2021-08-05 13:53:27 +0800

    [FIZZBUZZ][CARMELA]test: modified should_return_Whizz_when_count_off_given_multiple_by_seven_order_number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,35 +1,49 @@
 package tdd.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
-import static  org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class FizzBuzzTest {
     FizzBuzz fizzBuzz = new FizzBuzz();
-    @Test
-    void should_return_order_number_when_count_off_given_normal_case_order_number(){
-        int orderNumber = 1;
 
+    @Test
+    void should_return_order_number_when_count_off_given_normal_case_order_number() {
+        //given
+        int orderNumber = 1;
+        //when
         String result = fizzBuzz.countOff(orderNumber);
+        //then
         assertThat(result).isEqualTo("1");
     }
 
     @Test
-    void should_return_Fizz_when_count_off_given_multiple_by_three_order_number(){
+    void should_return_Fizz_when_count_off_given_multiple_by_three_order_number() {
+        //given
         int orderNumber = 9;
+        //when
         String result = fizzBuzz.countOff(orderNumber);
+        //then
         assertThat(result).isEqualTo("Fizz");
     }
 
     @Test
-    void should_return_Fizz_when_count_off_given_multiple_by_five_order_number(){
+    void should_return_Buzz_when_count_off_given_multiple_by_five_order_number() {
+        //given
         int orderNumber = 10;
+        //when
         String result = fizzBuzz.countOff(orderNumber);
+        //then
         assertThat(result).isEqualTo("Buzz");
     }
+
     @Test
-    void should_return_Fizz_when_count_off_given_multiple_by_seven_order_number(){
-        int orderNumber = 10;
+    void should_return_Whizz_when_count_off_given_multiple_by_seven_order_number() {
+        //given
+        int orderNumber = 7;
+        //when
         String result = fizzBuzz.countOff(orderNumber);
+        //then
         assertThat(result).isEqualTo("Whizz");
     }
 }
